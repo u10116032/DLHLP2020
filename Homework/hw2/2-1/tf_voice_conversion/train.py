f3134d4dcc3c65ec59b937224594e1403f6edf93
Halley <u10116032@gmail.com>
2020-05-05 21:24:59 +0800

    minor bugs fixed.

--- Homework/hw2/2-1/tf_voice_conversion/train.py
+++ Homework/hw2/2-1/tf_voice_conversion/train.py
@@ -5,6 +5,7 @@ import argparse
 import yaml
 import glob
 import os
+from tqdm import tqdm
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', help='Path to dataset folder',
@@ -15,7 +16,7 @@ parser.add_argument('--logdir', help='Log Path', default='./ckpt')
 def main():
   args = parser.parse_args()
   filenames = glob.glob(os.path.join(args.dataset,'*.tfrecord'))
-  dataset = dataset_builder.build(filenames)
+  dataset = dataset_builder.build(filenames, prefetch=1, batch=2)
   model = AutoEncoder()
   optimizer = tf.keras.optimizers.Adam()
   loss_object = tf.keras.losses.Huber(reduction=tf.keras.losses.Reduction.NONE)
@@ -23,7 +24,6 @@ def main():
   ckpt = tf.train.Checkpoint(model=model)
   ckpt_mgr = tf.train.CheckpointManager(ckpt, args.logdir, max_to_keep=5)
 
-  @tf.function
   def train_step(origin, speaker_one_hot, target):
     with tf.GradientTape() as tape:
       convert = model(origin, speaker_one_hot)
@@ -37,12 +37,16 @@ def main():
     # Reset the metrics at the start of the next epoch
     train_loss.reset_states()
 
-    for feature in dataset:
+    for feature in tqdm(dataset):
       speaker_id = feature['speaker_id']
       speaker_one_hot = tf.one_hot(speaker_id, 2)
       mel = feature['mel']
       mel = tf.expand_dims(mel, axis=-1)
-      train_step(mel, speaker_one_hot, mel)
+      try:
+        train_step(mel, speaker_one_hot, mel)
+      except Exception as e:
+        print(e)
+        print(mel.shape)
     ckpt_mgr.save()
     template = 'Epoch {}, Loss: {}'
     print(template.format(epoch + 1, train_loss.result()))
