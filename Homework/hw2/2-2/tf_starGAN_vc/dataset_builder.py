cf4aeeec0f00571e2fe53c3dfa5b3f9944c8db30
Halley <u10116032@gmail.com>
2020-05-17 23:39:33 +0800

    update params

--- Homework/hw2/2-2/tf_starGAN_vc/dataset_builder.py
+++ Homework/hw2/2-2/tf_starGAN_vc/dataset_builder.py
@@ -49,7 +49,7 @@ def build(filepaths, batch=16, prefetch=4):
   dataset = dataset.interleave(lambda x:
       tf.data.TFRecordDataset(x).map(_parse_feature,
       num_parallel_calls=len(filepaths)),
-      cycle_length=4, block_length=4)
+      cycle_length=8, block_length=8)
   # dataset = dataset.repeat(-1)
   dataset = dataset.shuffle(buffer_size=batch * prefetch)
   dataset = dataset.padded_batch(batch_size=batch, padded_shapes=pad_shape,
